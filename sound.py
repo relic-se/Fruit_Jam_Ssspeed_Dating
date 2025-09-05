a64e1c13ff3d60e241f74930496d5950a3d4e8f4
Cooper Dalrymple <dcooperdalrymple@gmail.com>
2025-09-05 14:52:24 -0500

    Fix dac present checks

--- sound.py
+++ sound.py
@@ -10,35 +10,36 @@ import adafruit_pathlib as pathlib
 import hardware
 
 VOICE = None
+DAC_PRESENT = hardware.peripherals.dac is not None
 
 # load sfx wave files
-SFX_CLICK = audiocore.WaveFile("sounds/click.wav") if hardware.tlv320_present else None
-SFX_BUZZER = audiocore.WaveFile("sounds/buzzer.wav") if hardware.tlv320_present else None
+SFX_CLICK = audiocore.WaveFile("sounds/click.wav") if DAC_PRESENT else None
+SFX_BUZZER = audiocore.WaveFile("sounds/buzzer.wav") if DAC_PRESENT else None
 
 def play_music(name:str="") -> None:
-    if hardware.tlv320_present:
+    if DAC_PRESENT:
         stop_music()
         path = pathlib.Path("sounds/music{:s}.wav".format("-" + name if len(name) else ""))
         if path.exists():
             hardware.mixer.play(audiocore.WaveFile(path.absolute()), voice=0, loop=True)
 
 def stop_music() -> None:
-    if hardware.tlv320_present:
+    if DAC_PRESENT:
         hardware.mixer.voice[0].stop()
 
 def play_sfx(wave:audiocore.WaveFile) -> None:
-    if hardware.tlv320_present and wave is not None:
+    if DAC_PRESENT and wave is not None:
         hardware.mixer.play(wave, voice=1, loop=False)
 
 def play_voice() -> None:
-    if hardware.tlv320_present and VOICE is not None:
+    if DAC_PRESENT and VOICE is not None:
         wave = VOICE[random.randint(0, len(VOICE)-1)]
         if wave is not None:
             hardware.mixer.play(wave, voice=2, loop=False)
 
 def load_voice(name:str) -> None:
     global VOICE
-    if hardware.tlv320_present:
+    if DAC_PRESENT:
         unload_voice()
         if len(name):
             path = pathlib.Path("sounds/{:s}".format(name))
@@ -52,13 +53,13 @@ def load_voice(name:str) -> None:
 
 def unload_voice() -> None:
     global VOICE
-    if hardware.tlv320_present:
+    if DAC_PRESENT:
         hardware.mixer.voice[2].stop()
         if VOICE is not None:
             del VOICE
             VOICE = None
 
 def is_voice_playing() -> bool:
-    if hardware.tlv320_present:
+    if DAC_PRESENT:
         return hardware.mixer.voice[2].playing
     return False
